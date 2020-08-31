f73a452cf94390e81b42db6bb8c3dec061cec424
mykelcodex <michealoke39@yahoo.com>
2020-08-31 22:45:45 +0100

    Go Live

--- pages/index.vue
+++ pages/index.vue
@@ -220,7 +220,7 @@ head() {
       title: 'Oke Michael - Software Engineer',
       meta: [
         { hid: 'description', name: 'description', content: 'Hello! My name is Oke Michael, i am a software engineer based in Nigeria. I spend most of time on the web which means i focus majorly on the web.' },
-        { name: 'keywords', content: ['Oke Michael','Mykelcodex','Michael Oke','Software Engineer in Nigeria','Software','Laravel Developer','Vuejs Developer','Open Source','Develop Community','PHP','Javascript'] },
+        { name: 'keywords', content: ['Oke Michael','Mykelcodex','Mykeel','Michael Oke','Software Engineer in Nigeria','Software','Laravel Developer','Vuejs Developer','Open Source','Develop Community','PHP','Javascript'] },
         { name: 'robots', content: 'all' },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'Oke Michael - Software Engineer' },
